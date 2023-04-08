200e4e907a685aed7a94453b4202d64959aa9620
lsj <lsj930224@naver.com>
2023-04-08 18:45:26 +0900

    영속컨텍스트/플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -37,22 +37,48 @@ public class JpaMain {
             //findMember.setName("HelloJPA");
 
             //JPQL : 객체를 대상으로 검색하는 객체 지향 쿼리, 객체 지향 SQL
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(5).setMaxResults(8)   //페이징
                     .getResultList();
 
             for(Member member : result){
                 System.out.println("member.name = " + member.getName());
-            }
+            }*/
+
+            //비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);  //1차 캐시에 저장됨
+            System.out.println("=== AFTER ===");
+
+            em.flush(); //commit전에 바로 DB반영
+            //영속성 컨텍스트를 비우지않음
+            //영속성 컨텍스트의 변경내용을 데이터베이스에 동기화
+
+            Member findMember = em.find(Member.class, 101L);  //1차 캐시에서 조회
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
 
+            //수정? update?
+            Member findMember1 = em.find(Member.class, 150L);
+            findMember1.setName("ZZZZZ");
+            //그냥 이렇게하면 update 쿼리 날려줌
 
+            //영속성 컨텍스트에서 관리하지마라라는 뜻(준영속 상태로 전환)
+            //update 쿼리를 날리지않음
+            em.detach(findMember1);
 
-
-
+            //영속성 컨텍스트를 완전 초기화
+            em.clear();
 
 
             //트랜잭션 commit
+            //commit하는 순간 DB에 insert
             tx.commit();
         }catch (Exception e){
             tx.rollback();
